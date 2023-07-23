c2caeae366f539f07d3116a8d2d73dcc2a5b41a6
Hammad-star <bscs2012293@szabist.pk>
2023-07-23 14:38:21 +0500

    Expense Item created
    ExpenseItem.js and ExpenseItem.css created and imported to App.js

---
+++ src/component/ExpenseItem.js
@@ -0,0 +1,18 @@
+import "./ExpenseItem.css";
+
+function ExpenseItem(props) {
+  const title = props.title;
+  const price = props.amount;
+  const date = props.date.toISOString();
+
+  return (
+    <div className="expense-item">
+      <div>{date}</div>;
+      <div className="expense-item__description">
+        <h2>{title}</h2>;<div className="expense-item__price">${price}</div>;
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
